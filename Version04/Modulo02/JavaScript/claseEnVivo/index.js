d04f731d72a7b8a9ab87dccd36d1c45dc8b5c969
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-11 19:12:51 -0500

    update live session javascript

---
+++ Version04/Modulo02/JavaScript/claseEnVivo/index.js
@@ -0,0 +1,39 @@
+//Fundamentos ds JS
+//Lenguaje de programación imperativo.
+//Lenguaje de programación interpretado.
+//Lenguaje de programación debilmente tipado.
+//Lenguaje de programación de un único hilo.
+//Lenguaje de programación multiplataforma.
+
+var primeraVariable = 5;
+primeraVariable = "Hola Mundo!";
+primeraVariable = true;
+primeraVariable = {};
+
+//Tipos de datos en JS
+//Primitivos y no primitivos
+//Tipos de datos primitivos o elementales son los datos orignales
+//de un lenguaje de programación, aquellos que nos proporciona el
+//lenguaje de programación
+//TIpos primitivos:
+//Undefined
+//Boolean
+//Number
+//String
+//Null
+//Object
+//Function
+//BigInit
+//symbol
+
+//typeof
+
+//Undefined y Null significan ausencia de valor
+//Undefined es ausencia de valor de forma implicita
+//Null es ausencia de valor de forma explicita
+//Declaro la variable y le asigno el valor de nulo(ausencia de valor)
+var primeraVariable = null;
+//Declaro la variable pero nunca la inicializo(ausencia de valor)
+var primeraVariable;
+
+//undefined
