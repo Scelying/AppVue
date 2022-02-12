83c65461e61ba92885744e2ecdcf010a448ade3b
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-11 19:42:27 -0500

    update live session javascript

--- Version04/Modulo02/JavaScript/claseEnVivo/index.js
+++ Version04/Modulo02/JavaScript/claseEnVivo/index.js
@@ -5,35 +5,56 @@
 //Lenguaje de programación de un único hilo.
 //Lenguaje de programación multiplataforma.
 
-var primeraVariable = 5;
-primeraVariable = "Hola Mundo!";
-primeraVariable = true;
-primeraVariable = {};
+//Como crear una variable en JS
+//Sintaxis -> var [<variable>] = [<valor>];
+var ejemplo = 10;
 
 //Tipos de datos en JS
 //Primitivos y no primitivos
 //Tipos de datos primitivos o elementales son los datos orignales
 //de un lenguaje de programación, aquellos que nos proporciona el
 //lenguaje de programación
-//TIpos primitivos:
+//Tipos primitivos:
 //Undefined
+//Null
 //Boolean
 //Number
 //String
-//Null
 //Object
 //Function
 //BigInit
 //symbol
 
 //typeof
+//saber cual es el tipo de una variable en JS
+//Sintaxis -> typeof [<variable>] === [<tipoDato>]
+if(typeof primeraVariable === 'undefined') {
+    console.log("Es un undefined");
+}
+else {
+    console.log("No es un undefined");
+}
+
+//Comprarciones == y ===
+// == (Comparación Sencilla)
+//Compara el valor
+console.log(0 == false);
+
+// === (Comparación Esctricta)
+//Compara el valor y el tipo de dato
+console.log(5 === "5");
 
 //Undefined y Null significan ausencia de valor
 //Undefined es ausencia de valor de forma implicita
-//Null es ausencia de valor de forma explicita
-//Declaro la variable y le asigno el valor de nulo(ausencia de valor)
-var primeraVariable = null;
+//undefined
 //Declaro la variable pero nunca la inicializo(ausencia de valor)
+//Sintaxis -> var [<variable>]
 var primeraVariable;
 
-//undefined
+//Null
+//Null es ausencia de valor de forma explicita
+//Declaro la variable y le asigno el valor de nulo(ausencia de valor)
+//Sintaxis -> var [<variable>] = null
+var primeraVariable = null;
+
+
