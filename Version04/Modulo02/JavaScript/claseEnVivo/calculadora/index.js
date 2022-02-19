90030477eee6491e1c1c0b0619064812fbf8b60b
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-19 13:11:16 -0500

    update DOM

--- Version04/Modulo02/JavaScript/claseEnVivo/calculadora/index.js
+++ Version04/Modulo02/JavaScript/claseEnVivo/calculadora/index.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo');
+var sumando1 = Number(document.getElementById('sumando1').innerText);
+var sumando2 = Number(document.getElementById('sumando2').innerText);
+
+cambioSumando1 = (valor) => {
+    sumando1 = Number(valor);
+}
+
+cambioSumando2 = (valor) => {
+    sumando2 = Number(valor);
+}
+
+sumar = () => {
+    var suma = sumando1 + sumando2;
+    var contenedor = document.createElement('p');
+    var contenido = document.createTextNode(suma);
+    contenedor.appendChild(contenido);
+    var boton = document.getElementsByTagName('body');
+    var posicionEnElDocumento = boton[0];
+    posicionEnElDocumento.appendChild(contenedor);
+}
