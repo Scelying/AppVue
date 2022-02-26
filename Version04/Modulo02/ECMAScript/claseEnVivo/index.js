6291d12dd165b78ade0dcea98c5b0f0fa5a4a9ae
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-25 20:00:24 -0500

    update ES

--- Version04/Modulo02/ECMAScript/claseEnVivo/index.js
+++ Version04/Modulo02/ECMAScript/claseEnVivo/index.js
@@ -52,8 +52,9 @@ var texto = '';
 var arregloNumeros = [2,4,6,8,0];
 arregloNumeros.forEach(multiplicarPorDos);
 
-function multiplicarPorDos(valor){
+function multiplicarPorDos(valor, posicion, arreglo){
     texto = texto + valor*2 + "\n";
+    //console.log("valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
 }
 //console.log(texto);
 
@@ -61,12 +62,50 @@ function multiplicarPorDos(valor){
 //<nombreArreglo>.map(<metodo>)
 //Crea un nuevo arreglo a partir de otro arreglo
 var numero = arregloNumeros.map(multiplicarPorTres);
-console.log(numero);
+//console.log(numero);
 
-function multiplicarPorTres(valor){
+function multiplicarPorTres(valor, posicion, arreglo){
+    //console.log("valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
     return valor*3;
 }
 
 //filter()
 //<nombreArreglo>.filter(<metodo>)
 //Crea un nuevo arreglo a partir de una condicion
+var numerosImpares = arregloNumeros.filter(numerosImparesMetodo);
+//console.log(numerosImpares);
+
+function numerosImparesMetodo(valor, posicion, arreglo) {
+    //console.log("valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
+    return (valor % 2 === 1);
+};
+
+//reduce()
+//Retorna un valor
+var sumaArreglo = arregloNumeros.reduce(sumaNumerosMetodo);
+//console.log(sumaArreglo);
+
+function sumaNumerosMetodo(total, valor) {
+    //console.log("total: ", total, "valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
+    return total + valor;
+}
+
+//every()
+//Devuelve true si se cumpla para todo el arreglo una condicion
+var numerosPares = arregloNumeros.every(numerosParesMetodo);
+//console.log(numerosPares);
+
+function numerosParesMetodo(valor, posicion, arreglo) {
+    //console.log("valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
+    return (valor % 2 === 0);
+}
+
+//some()
+//Devuelve un true si se cumpla para al menos un indice una condicion
+var numerosMayoresA7 = arregloNumeros.some(numerosMayoresA7Metodo);
+console.log(numerosMayoresA7);
+
+function numerosMayoresA7Metodo(valor, posicion, arreglo) {
+    //console.log("valor: ", valor, "posicion: ", posicion, "arreglo:", arreglo);
+    return valor > 7;
+}
