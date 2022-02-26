557ccfb4e39f9302615d371ea91f3cdf9638f768
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-26 10:37:50 -0500

    update ES7

--- Version04/Modulo02/ECMAScript/claseEnVivo/ES7.js
+++ Version04/Modulo02/ECMAScript/claseEnVivo/ES7.js
@@ -1,1 +1,13 @@
-console.log('ES7');
+//Operador Exponencial de JS
+//Notacion -> **
+console.log(5 ** 3);
+
+//Asiganción de exponenciación
+let numero = 5;
+numero **= 2;
+console.log(numero);
+
+//includes()
+//Nos permite verificar si un elemento está presente en un arreglo
+const arregloNombrePaises = ['Colombia', 'Peru', 'Bolivia', 'Brasil'];
+console.log(arregloNombrePaises.includes('Colombia'));
