abb9f557de32c734119a4dd7a9cfd85936876d92
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-26 12:31:37 -0500

    update ES11

--- Version04/Modulo02/ECMAScript/claseEnVivo/ES11.js
+++ Version04/Modulo02/ECMAScript/claseEnVivo/ES11.js
@@ -1,1 +1,38 @@
-console.log('ES11');
+//ES11
+//Operador Opcional
+//Sintaxis -> ?
+const vehiculos = {
+    automoviles: [
+        {
+            marca: 'Ford'
+        },
+        {
+            marca: 'Audi'
+        }
+    ]
+};
+
+//console.log(vehiculos.automoviles[1].marca);
+//console.log(vehiculos.automoviles[2].marca);
+//console.log(vehiculos.automoviles[2]?.marca);
+
+//Nullish Coalescing
+//Sintaxis -> ||
+const numero = 7;
+const otroNumero = numero || 5;
+console.log(otroNumero);
+
+//Si es 0
+const numeroDos = 0;
+const otroNumeroDos = numeroDos || 5;
+console.log(otroNumeroDos);
+
+//??
+const numeroTres = 0;
+const otroNumeroTres = numeroTres ?? 5;
+console.log(otroNumeroTres);
+
+//??
+const numeroCuatro = null;
+const otroNumeroCuatro = numeroCuatro ?? 5;
+console.log(otroNumeroCuatro);
