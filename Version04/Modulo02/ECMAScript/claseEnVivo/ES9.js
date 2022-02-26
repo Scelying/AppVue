8931dc86cdecd425d91a59b4fc3ffb98e0ea12bc
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-26 11:52:36 -0500

    update ES9

--- Version04/Modulo02/ECMAScript/claseEnVivo/ES9.js
+++ Version04/Modulo02/ECMAScript/claseEnVivo/ES9.js
@@ -21,8 +21,23 @@ const promesaTres = new Promise((resolve, reject) => {
 
 const arregloPromesas = [promesaUno, promesaDos, promesaTres];
 
-(async function() {
+/*(async function() {
     for await (let elemento of arregloPromesas) {
         console.log(`elemento: ${elemento}`);
     }
-})();
+})();*/
+
+//Promise.finally()
+const promesaCuatro = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Exito Cuarta Promesa");
+    }, 1000)
+    /*setTimeout(() => {
+        reject("Error Cuarta Promesa");
+    }, 2000)*/
+});
+
+promesaCuatro
+.then((response) => console.log(response))
+.catch((error) => console.error(error))
+.finally(() => console.log('Se ejecuta esta linea'));
