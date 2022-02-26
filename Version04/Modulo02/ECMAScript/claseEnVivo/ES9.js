d9d8a0b8c5c40537a05a11e8bb92427da94cc26f
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-02-26 11:46:46 -0500

    update ES9

--- Version04/Modulo02/ECMAScript/claseEnVivo/ES9.js
+++ Version04/Modulo02/ECMAScript/claseEnVivo/ES9.js
@@ -1,1 +1,28 @@
-console.log('ES9');
+//Iteración Asincrona
+//Poder usar await en for/of
+//Sintaxis for await() {}
+const promesaUno = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Exito Primera Promesa");
+    }, 2000)
+});
+
+const promesaDos = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Exito Segunda Promesa");
+    }, 2000)
+});
+
+const promesaTres = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Exito Tercer Promesa");
+    }, 2000)
+});
+
+const arregloPromesas = [promesaUno, promesaDos, promesaTres];
+
+(async function() {
+    for await (let elemento of arregloPromesas) {
+        console.log(`elemento: ${elemento}`);
+    }
+})();
