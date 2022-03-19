3e0dc5e09192be81de8c51c3beeae4a2b24ce09b
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-03-19 08:13:52 -0500

    second commit

--- Version04/Modulo05/claseEnVivo/index.js
+++ Version04/Modulo05/claseEnVivo/index.js
@@ -1,3 +1,5 @@
+/*
+//Node
 //Importar modulos
 //require('<nombreModulo>');
 //require() -> La forma como node carga los modulos
@@ -13,7 +15,40 @@ console.log(http.Server);
 //levantar usando el metodo listen(<numeroPuerto>).
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end('{"mensaje": "mi primer mensaje"}');
-}).listen(8082);
+    response.end('{"mensaje": "mi segundo mensaje"}');
+}).listen(8081);
 
-console.log('Servidor esta corriendo en el puerto 8082');
+console.log('Servidor esta corriendo en el puerto 8081');
+*/
+
+//Express
+//Importamos y usamos express
+const express = require('express');
+//Creamos una instancia de express
+const app = express();
+//crear puerto
+const puerto = 5500;
+//Creamos un puerto para express
+app.post('/', (request, response) => {
+    //valla a db y traigame info
+    response.send('ruta para operacion crear verbo POST');
+});
+
+app.get('/', (request, response) => {
+    //valla a db y traigame info
+    response.send('ruta para operacion leer verbo GET');
+});
+
+app.put('/', (request, response) => {
+    //valla a db y traigame info
+    response.send('ruta para operacion actualizar verbo PUT');
+});
+
+app.delete('/', (request, response) => {
+    //valla a db y traigame info
+    response.send('ruta para operacion eliminar verbo DELETE');
+});
+
+app.listen(puerto, () => {
+    console.log(`Aplicacion arriba sobre el puerto ${puerto}`);
+})
