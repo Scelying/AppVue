0ba0c1ab34993d583841b7452b1fa0ec1104a8bb
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-03-26 10:02:53 -0500

    new structure

--- Version04/Modulo05/claseEnVivo/API/controladores/automoviles.js
+++ Version04/Modulo05/claseEnVivo/API/controladores/automoviles.js
@@ -22,3 +22,49 @@ exports.listarTodos = (req, response) => {
     response.json(resp);
   });
 };
+
+exports.listarPorModelo = (req, response) => {
+  const modelo = req.params.modelo;
+  automovilesModel.find({ modelo: modelo }, (err, resp) => {
+    response.json(resp);
+  });
+};
+
+exports.listarPorColor = (req, response) => {
+  const color = req.params.color;
+  automovilesModel.find({ color: color }, (err, resp) => {
+    response.json(resp);
+  });
+};
+
+exports.listarPorColorYModelo = (req, response) => {
+  const color = req.params.color;
+  const modelo = req.params.modelo;
+
+  automovilesModel.find({ color: color, modelo: modelo }, (err, resp) => {
+    response.json(resp);
+  });
+};
+
+exports.listarPorColorYModelo = (req, response) => {
+  const color = req.params.color;
+  const modelo = req.params.modelo;
+
+  automovilesModel.find({ color: color, modelo: modelo }, (err, resp) => {
+    response.json(resp);
+  });
+};
+
+exports.actualizarDatos = (req, response) => {
+  //valla a db y traigame info
+  response.send("ruta para operacion actualizar verbo PUT");
+};
+
+exports.eliminarDatos = (req, response) => {
+    //valla a db y traigame info
+    response.send('ruta para operacion eliminar verbo DELETE');
+};
+
+exports.paginaNoEncontrada = (req, response) => {
+  response.send('Pagina no encontrada', 404);
+};
--- Version04/Modulo05/claseEnVivo/API/rutas/index.js
+++ Version04/Modulo05/claseEnVivo/API/rutas/index.js
@@ -10,45 +10,18 @@ const automovilControlador = require('../controladores/automoviles');
 
 //Operacion para listar todos los datos
 rutas.get('/', automovilControlador.listarTodos);
-
-rutas.get('/modelo/:modelo', (req, response) => {
-    const modelo = req.params.modelo;
-    console.log('modelo: ', modelo);
-    automovilesModel.find({modelo: modelo}, (err, resp) => {
-        console.log(resp);
-        response.json(resp);
-    });
-});
-
-rutas.get('/color/:color', (req, response) => {
-    const color = req.params.color;
-    console.log('color: ', color);
-    automovilesModel.find({color: color}, (err, resp) => {
-        console.log(resp);
-        response.json(resp);
-    });
-});
-
-rutas.get('/color/:color([a-z]+)/modelo/:modelo([0-9]{4})', (req, res) => {
-    const color = req.params.color;
-    const modelo = req.params.modelo;
-
-    res.send(`/color/${color}/modelo/${modelo}`);
-});
-
-rutas.put('/', (request, response) => {
-    //valla a db y traigame info
-    response.send('ruta para operacion actualizar verbo PUT');
-});
-
-rutas.delete('/', (request, response) => {
-    //valla a db y traigame info
-    response.send('ruta para operacion eliminar verbo DELETE');
-});
-
-rutas.get('*', (req, res) => {
-    res.send('Pagina no encontrada', 404);
-})
+//Operacion para listar todos los datos por modelo
+rutas.get('/modelo/:modelo', automovilControlador.listarPorModelo);
+//Operacion para listar todos los datos por color
+rutas.get('/color/:color', automovilControlador.listarPorColor);
+//Operacion para listar todos los datos por color y modelo
+rutas.get('/color/:color([a-z]+)/modelo/:modelo([0-9]{4})', automovilControlador.listarPorColorYModelo);
+//Operacion para actualizar
+rutas.put('/', automovilControlador.actualizarDatos);
+//Operacion para eliminar
+rutas.delete('/', automovilControlador.eliminarDatos);
+//Operacion pagina no encontrada
+rutas.get('*', automovilControlador.paginaNoEncontrada);
 
 /*
 //Operacion para listar todos los datos
