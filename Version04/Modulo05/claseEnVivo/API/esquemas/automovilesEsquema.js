02b235d68795a5f3abd03f02ea15f6820a7fadd0
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-03-26 12:51:54 -0500

    add debug

--- Version04/Modulo05/claseEnVivo/API/esquemas/automovilesEsquema.js
+++ Version04/Modulo05/claseEnVivo/API/esquemas/automovilesEsquema.js
@@ -1,6 +1,10 @@
 //importar y usamos mongoose
 const mongoose = require("mongoose");
 
+mongoose.set("debug", function (coll, method, query, doc) {
+  console.log(query);
+});
+
 //Crear un esquema de Vehiculo
 exports.automovilSchema = mongoose.Schema(
   {
