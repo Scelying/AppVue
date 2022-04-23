7b14ad7743b726e921824c61df97658716d20b86
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-04-23 08:41:35 -0500

    update modulo06

--- Version04/Modulo06/Angular/miprimerapalicacion/src/app/app.module.ts
+++ Version04/Modulo06/Angular/miprimerapalicacion/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MiprimercomponenteComponent } from './miprimercomponente/miprimercomponente.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MiprimercomponenteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
