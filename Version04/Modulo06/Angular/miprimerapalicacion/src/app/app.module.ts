2493f4ae441e251554eea84a1394ab6f11ad97c9
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-04-23 09:47:59 -0500

    add material

--- Version04/Modulo06/Angular/miprimerapalicacion/src/app/app.module.ts
+++ Version04/Modulo06/Angular/miprimerapalicacion/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from '../material.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ArticleAComponent } from './article-a/article-a.component';
+import { ArticleBComponent } from './article-b/article-b.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ArticleAComponent,
+    ArticleBComponent
   ],
   imports: [
     BrowserModule,
---
+++ Version04/Modulo06/Angular/miprimerapalicacion/src/app/article-a/article-a.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArticleAComponent } from './article-a.component';
+
+describe('ArticleAComponent', () => {
+  let component: ArticleAComponent;
+  let fixture: ComponentFixture<ArticleAComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArticleAComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArticleAComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
