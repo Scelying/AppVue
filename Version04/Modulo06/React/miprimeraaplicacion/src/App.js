8c12f7a0d9be70ad24ee27ca71ef0670253a0a24
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-04-30 11:37:55 -0500

    new UX structure

--- Version04/Modulo06/React/miprimeraaplicacion/src/App.js
+++ Version04/Modulo06/React/miprimeraaplicacion/src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
 
 function App() {
+
+}
+
+function Header() {
   const title = 'Mi primera Aplicación';
   return(
-    <div class="lg:flex lg:items-center lg:justify-between">
-      <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{title}</h2>
-        <div class="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-      
+    <header>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+          <span className="font-semibold text-xl tracking-tight">{title}</span>
         </div>
-      </div>
-    </div>
+        <div classNameName="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Bienvenida
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Comunicación Componentes
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+              Consumo API
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
+}
+
+function Main() {
+  
+}
+
+function Footer() {
+  return(
+    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 Pepito Perez. Derechos reservados.
+      </span>
+    </footer>
+  )
 }
 
 export default App;
