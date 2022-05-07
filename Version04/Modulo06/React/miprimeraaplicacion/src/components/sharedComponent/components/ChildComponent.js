152f4593f85d24edd4b45bda875f414826f4c8cd
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-05-07 13:22:16 -0500

    add styles

--- Version04/Modulo06/React/miprimeraaplicacion/src/components/sharedComponent/components/ChildComponent.js
+++ Version04/Modulo06/React/miprimeraaplicacion/src/components/sharedComponent/components/ChildComponent.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const ChildComponent = ({ data }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [selected, setSelected] = useState(null);
 
   useEffect(() => {
     if (data.length > 0) {
@@ -10,8 +11,10 @@ const ChildComponent = ({ data }) => {
     }
   }, [data]);
 
-  const handleChange = () => {
-    console.log("onChange");
+  const handleChange = (event) => {
+    let vehiculoSeleccionado = data.filter(dataTemporal => dataTemporal.id === parseInt(event.target.value));
+    console.log(vehiculoSeleccionado[0]);
+    setSelected(vehiculoSeleccionado[0]);
   };
 
   return (
@@ -24,18 +27,29 @@ const ChildComponent = ({ data }) => {
         </div>
       )}
       {!isLoading && (
-        <select className="form-select form-select-sm
-        appearance-none
-        block
-        w-full"
-        multiple={true} value={data} onChange={handleChange}>
-          {data.map((vehicle) => (
-            <option key={vehicle.id} value={vehicle.nombre}>
-              {vehicle.nombre}
-            </option>
-          ))}
-        </select>
+        <div className="py-60">
+          <div className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <select
+              className="w-full"
+              multiple={true}
+              value={data}
+              onChange={handleChange}
+            >
+              {data.map((vehicle) => (
+                <option key={vehicle.id} value={vehicle.id}>
+                  {vehicle.nombre} - {vehicle.medio}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
       )}
+      {
+        !isLoading && !selected && <p className="font-extrabold text-center">No ha seleccionado ninguna opción</p>
+      }
+      {
+        !isLoading && selected && <p className="font-extrabold text-center">Usted selecciono: {selected.nombre} - {selected.medio}</p>
+      }
     </>
   );
 };
