70aa1d422aba8284af08514792ed9be5eb288944
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-05-07 12:07:07 -0500

    add styles

--- Version04/Modulo06/React/miprimeraaplicacion/src/components/pageNotFound/PageNotFound.js
+++ Version04/Modulo06/React/miprimeraaplicacion/src/components/pageNotFound/PageNotFound.js
@@ -1,7 +1,11 @@
 const PageNotFound = () => {
-    return(
-        <p>Lo sentimos página no encontrada</p>
-    )
-}
+  return (
+    <div className="py-72">
+      <div className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <p className="text-center">Lo sentimos página no encontrada</p>
+      </div>
+    </div>
+  );
+};
 
 export default PageNotFound;
--- Version04/Modulo06/React/miprimeraaplicacion/src/components/welcome/Welcome.js
+++ Version04/Modulo06/React/miprimeraaplicacion/src/components/welcome/Welcome.js
@@ -1,7 +1,11 @@
 const Welcome = () => {
-    return(
-        <p>Bienvenidos A mi aplicación</p>
-    )
-}
+  return (
+    <div className="py-72">
+      <div className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <p className="text-center">Bienvenidos A mi aplicación</p>
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
