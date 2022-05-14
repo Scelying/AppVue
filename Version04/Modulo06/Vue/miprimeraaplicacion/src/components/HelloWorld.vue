6bd32dbb7eb59650d72e10a7089f2c39d8261f38
Gustavo Garcia <gustavogarcia@Gustavos-MacBook-Air.local>
2022-05-13 22:08:22 -0500

    update style

--- Version04/Modulo06/Vue/miprimeraaplicacion/src/components/HelloWorld.vue
+++ Version04/Modulo06/Vue/miprimeraaplicacion/src/components/HelloWorld.vue
@@ -3,9 +3,11 @@
     <span v-once>{{ nombre }} - {{ apellido }}</span>
     <p v-html="html"></p>
     <p v-if="nombre === 'Gustavo'">Soy Yo</p>
-    <v-btn class="ma-2" color="primary" @click="alerta" dark>
+    <v-btn class="ma-2" color="primary" @click="alerta('Hola')" dark>
       Generar alerta
     </v-btn>
+    <span>{{split}}</span>
+    <span>{{reversed}}</span>
   </v-container>
 </template>
 
@@ -14,15 +16,28 @@ export default {
   name: "HelloWorld",
   data: () => ({
     html: '<span style="color:red">Hola Mundo</span>',
+    arreglo: [5,4,3,2,1]
   }),
   props: {
     nombre: String,
     apellido: String,
   },
   methods: {
-    alerta: function () {
-      alert("Hola mundo");
+    alerta: (parametro) => {
+      alert(parametro + " mundo");
     },
+    reversedName1:() => {
+      return this.html + ' metodo';
+    }
   },
+  computed: {
+    split() {
+      return this.html.split(' ');
+    },
+    reversed() {
+      let copia = this.arreglo;
+      return copia.reverse();
+    }
+  }
 };
 </script>
